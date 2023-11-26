09b58ce9c893d28487329c61474d78de5cc0e227
tohidi <s.tohidi22@gmail.com>
2023-11-26 14:31:22 +0330

    initial

---
+++ dashboard.py
@@ -0,0 +1,22 @@
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from grappelli.dashboard import modules, Dashboard
+
+
+class CustomIndexDashboard(Dashboard):
+    def init_with_context(self, context):
+        self.children.append(
+            modules.ModelList(
+                title="User",
+                column=1,
+                models=("users.models.*",),
+            )
+        )
+
+        self.children.append(
+            modules.ModelList(
+                _("Administration"),
+                column=1,
+                models=("django.contrib.*",),
+            )
+        )
